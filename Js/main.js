4533916e36e74232d8fb808b546cf29e4fa0e57b
Marihan Itzel <marihan.bernal@gmail.com>
2022-05-26 12:13:07 -0500

    Se agrega validacion de algoritmos 1 y 2

--- Js/main.js
+++ Js/main.js
@@ -1,168 +1,188 @@
-
-
 function validacion(dato){
 
     if ((dato>100) || (dato<1)){
         ingreso();
     }
     else{
-        return dato;
+        num = dato;
     }
+    return num;
 }
 
-function ingreso (){
+function ingreso ( ){
     let dato = parseFloat(prompt("Ingresa un número ENTERO entre 1 y 100")); 
     validacion(dato);
+    return num;
 }
 
 
+function Algoritmo1 (){
+    // Solicitar 3 numeros entre 1 y 100 y definir cual es el mayor
 
-// Solicitar 3 numeros entre 1 y 100 y definir cual es el mayor
+    alert("Algoritmo para definir el número mayor");
 
-alert("Algoritmo para definir el número mayor");
+    let num = 0;
+    let a = ingreso( );
+    alert("ok");
+   
 
+    let b = ingreso();
+    alert("ok");
+    
 
-let a = ingreso();
-alert("ok");
-console.log(a);
-
-ingreso();
-let b = validacion();
-alert("ok");
-console.log(b);
-
-ingreso();
-let c = validacion();
-alert("ok");
-console.log(c);
+    let c = ingreso();
+    alert("ok");
+   
 
 
+        if ((a>b) && (a>c)){
 
-    if ((a>b) && (a>c)){
+            alert("El número mayor es : "+a);
+        }
 
-        alert("El número mayor es :  "+a);
+        else if  ((b>a) && (b>c)){
+            
+            alert("El número mayor es : "+ b);
+        }
+
+        else if ((a==b) && (b==c)){
+
+            alert("Los números son iguales : "+ b);
+        }
+
+        else{
+            alert("El número mayor es : " +c);
+        }
+
+}
+
+
+function Algoritmo2 (){
+    alert("Algoritmo para definir el número menor");
+    let num = 0;
+    let d = ingreso( );
+    alert("ok");
+    
+
+    let e = ingreso();
+    alert("ok");
+    
+
+    let f = ingreso();
+    alert("ok");
+    
+
+    if ((d<e) && (d<f)){
+        alert("El número menor es : " +d);
     }
 
-    else if  ((b>a) && (b>c)){
+    else if  ((e<d) && (e<f)){
         
-        alert("El número mayor es : "+ b);
+        alert("El número menor es : " +e);
     }
 
-    else if ((a==b) && (b==c)){
+    else if ((e==d) && (e==f)){
 
-        alert("Los números son iguales :"+ b);
+        alert("Los números son iguales : "+ b);
     }
 
     else{
-        alert("El número mayor es" +c);
+        alert("El número menor es : "+ f);
+    }
+}
+
+
+
+function Algoritmo4(){
+    // ALGORITMO 4
+    alert("Algoritmo para definir si un número es múltiplo de 3");
+
+
+    function ingreso1 (){
+        let a = parseFloat(prompt("Ingresa un número ENTERO entre 100 y 200")); 
+        validacion1(a);
+    }
+
+    function validacion1(a){
+        if ((  (a<100) || (a>200)) || (a%1!=0) ) {
+            ingreso1();
+        }
+        else{
+            if (a%3==0){
+                alert("El número " +a+ " sí es múltiplo de 3")
+            }
+            else {
+                alert("El número " +a+ " no es múltiplo de 3")
+            }
+            
+        }
+    }
+
+    ingreso1();
+
+}
+
+
+function Algoritmo5() {
+    // ALGORITMO 5
+
+    alert("Algoritmo para definir si un número es la suma de los otros dos números dados");
+
+
+        let a1 = parseFloat(prompt("Ingresa un número")); 
+        let b1 = parseFloat(prompt("Ingresa un número")); 
+        let c1 = parseFloat(prompt("Ingresa un número")); 
+
+
+    let sumaab = a1+b1;
+    let sumaac = a1+c1;
+    let sumabc = b1+c1;
+
+
+    if ( (sumaab==c1) ) {
+        alert ("La suma de los dos primeros números ingresados es igual al último número ingresado "+ a1+"+"+b1+"="+c1);
+    }
+
+    else if ( (sumaac==b1)) {
+        alert ("La suma del primer y último número ingresado es igual al segundo número ingresado "+ a1+"+"+c1+"="+b1);
+    }
+
+    else if ((sumabc==a1) ) {
+        alert ("La suma de los últimos dos números ingresados es igual al primer número ingresado "+ b1+"+"+c1+"="+a1);
+    }
+
+    else{
+        alert("Ninguno de los números ingresados es igual a la suma de los otros dos números");
+    }
+
     }
 
 
 
-// alert("Algoritmo para definir el número menor");
-// let d = parseFloat(prompt("Ingresa un número entre 1 y 100")); 
-// let e = parseFloat(prompt("Ingresa un número entre 1 y 100")); 
-// let f = parseFloat(prompt("Ingresa un número entre 1 y 100")); 
+function Algoritmo6() {
+    // ALGORITMO 6
+
+    alert("Algoritmo para definir si un número es par o impar");
 
 
-// if ((d<e) && (d<f)){
-//     alert("El número menor es : " +d);
-// }
+        let nume = parseFloat(prompt("Ingresa un número"));
 
-// else if  ((e<d) && (e<f)){
-    
-//     alert("El número menor es : " +e);
-// }
-
-// else{
-//     alert("El número menor es : "+ f);
-// }
-
+        if ( (nume%2==0) ) {
+            alert("El número " +nume+ " es par")
+        }
+            
+        else{
+            alert("El número " +nume+ " es impar")
+        }
+        
+    }
 
 
-
-
-// // ALGORITMO 4
-// alert("Algoritmo para definir si un número es múltiplo de 3");
-
-
-// function ingreso (){
-//     let a = parseFloat(prompt("Ingresa un número ENTERO entre 100 y 200")); 
-//     validacion(a);
-// }
-
-// function validacion(a){
-//     if ((  (a<100) || (a>200)) || (a%1!=0) ) {
-//         ingreso();
-//     }
-//     else{
-//         if (a%3==0){
-//             alert("El número " +a+ " sí es múltiplo de 3")
-//          }
-//          else {
-//              alert("El número " +a+ " no es múltiplo de 3")
-//          }
-         
-//     }
-// }
-
-// ingreso();
-
-
-
-
-// // ALGORITMO 5
-
-// alert("Algoritmo para definir si un número es la suma de los otros dos números dados");
-
-
-//     let a = parseFloat(prompt("Ingresa un número")); 
-//     let b = parseFloat(prompt("Ingresa un número")); 
-//     let c = parseFloat(prompt("Ingresa un número")); 
-
-
-// let sumaab = a+b;
-// let sumaac = a+c;
-// let sumabc = b+c;
-
-
-// if ( (sumaab==c) ) {
-//     alert ("La suma de los dos primeros números ingresados es igual al último número ingresado "+ a+"+"+b+"="+c);
-// }
-
-// else if ( (sumaac==b)) {
-//     alert ("La suma del primer y último número ingresado es igual al segundo número ingresado "+ a+"+"+c+"="+b);
-// }
-
-// else if ((sumabc==a) ) {
-//     alert ("La suma de los últimos dos números ingresados es igual al primer número ingresado "+ b+"+"+c+"="+a);
-// }
-
-// else{
-//     alert("Ninguno de los números ingresados es igual a la suma de los otros dos números");
-// }
-
-
-
-
-
-
-// // ALGORITMO 6
-
-// alert("Algoritmo para definir si un número es par o impar");
-
-
-//     let num = parseFloat(prompt("Ingresa un número"));
-
-//     if ( (num%2==0) ) {
-//         alert("El número " +num+ " es par")
-//     }
-           
-//     else{
-//         alert("El número " +num+ " es impar")
-//      }
-    
-
+    Algoritmo1();
+    Algoritmo2();
+    Algoritmo4();
+    Algoritmo5();
+    Algoritmo6();
 
 
 
